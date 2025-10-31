0f5dfccf5693a7722825070c34c9def8de99ad58
mohd-ksr <mohd.ksr2003@gmail.com>
2025-10-31 15:44:37 +0530

    Initial project setup (frontend + backend)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Task {
+  _id: string;
+  title: string;
+}
+
+function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    axios.get("https://your-backend-url.onrender.com/api/tasks")
+      .then(res => setTasks(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Team Task Tracker</h1>
+      <ul>
+        {tasks.map((t) => (
+          <li key={t._id}>{t.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
